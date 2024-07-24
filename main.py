21e017314a555c3f07156977774dabed4079f517
teafany <Tiffany121705@gmail.com>
2024-07-24 12:31:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def encode(password):
         new_char = str((int(char) + 3) % 10)
         encoded_password += new_char
     return encoded_password
+
+def decode(password):
+    decoded_password = ""
+    for char in password:
+        new_char = str((int(char) - 3) % 10)
+        decoded_password += new_char
+    return decoded_password
+    
 def main():
     while True:
         print()
