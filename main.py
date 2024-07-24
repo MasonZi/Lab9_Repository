f58c7c1c878fef642f7295c59930062c385e5800
mason zilch <masonzilch@gmail.com>
2024-07-24 11:44:14 -0400

    This is my first addition to the repo

---
+++ main.py
@@ -0,0 +1,28 @@
+:#Mason Zilch Lab 9
+
+def encode(password):
+    encoded_password = ""
+    for char in password:
+        # Convert the character to an integer, add 3, and then convert it back to a string
+        new_char = str((int(char) + 3) % 10)
+        encoded_password += new_char
+    return encoded_password
+def main():
+    while True:
+        print()
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        pick = int(input("Please enter an option: ")
+
+        if pick == 1:
+            password = int(input("Please enter your password to encode: "))
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+
+
+if __name__ == "__main__":
+    main()
